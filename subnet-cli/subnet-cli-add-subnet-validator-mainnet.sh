5572873e60b0f76b927b596cc77cf222750d454c
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2023-02-01 00:20:41 +0800

    fix: subnet-cli needs to add public URI when adding validator to mainnet
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

--- subnet-cli/subnet-cli-add-subnet-validator-mainnet.sh
+++ subnet-cli/subnet-cli-add-subnet-validator-mainnet.sh
@@ -5,4 +5,6 @@ SUBNET_ID="2gHgAgyDHQv7jzFg6MxU2yyKq5NZBpwFLFeP8xX2E3gyK1SzSQ"
 
 subnet-cli add subnet-validator \
     --node-ids="${NODE_ID}" \
-    --subnet-id="${SUBNET_ID}"
+    --subnet-id="${SUBNET_ID}" \
+    --public-uri "https://api.avax.network"
+
